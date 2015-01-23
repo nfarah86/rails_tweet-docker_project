d3045d6f6184f37f7bc33ac6395dfeda2ec5446d
Nadine <nfarah86@gmail.com>
2015-01-23 14:06:05 -0800

    working on static pages and TDD

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,28 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get about" do
+  	get :about
+  	assert_response :success
+  end
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", Home
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", help
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", About
+  end
+
+
 end

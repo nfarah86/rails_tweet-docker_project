5c9aa0f923a2b5e56d6a54dd37c5d4778f131f15
Nadine <nfarah86@gmail.com>
2015-01-27 13:47:48 -0800

    modified password validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -47,7 +47,7 @@ class UserTest < ActiveSupport::TestCase
 
  	test "email validation should reject invalid addresses" do
 	    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-	                           foo@bar_baz.com foo@bar+baz.com]
+	                           foo@bar_baz.com foo@bar+baz.com foo@bar....com]
 	    invalid_addresses.each do |invalid_address|
 	      @user.email = invalid_address
 	      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -60,6 +60,14 @@ class UserTest < ActiveSupport::TestCase
 		@user.save
 		assert_not duplicate_user.valid?
 	end
+
+	test "email addresses should be saved as lower-case" do
+		mixed_case_email = "FoO@examPLE.cOM"
+		@user.email = mixed_case_email
+		@user.save
+		assert_equal mixed_case_email.downcase, @user.reload.email
+	end
+
 
 	test "password should have a minimum length" do
 		@user.password = @user.password_confirmation = "a" * 5

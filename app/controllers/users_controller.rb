34cc12e7db4bbdac199c01d5724e582c79065065
Nadine <nfarah86@gmail.com>
2015-01-28 16:16:15 -0800

    worked on routes, css, integration tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_attributes)
   	if @user.save
+  		flash[:success] = "Welcome " + "#{@user.name}" + "!"
+  		redirect_to @user
   	
   	else
   		render 'new'

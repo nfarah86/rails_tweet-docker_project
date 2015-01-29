763b03e68f418ac81d77f0708f12b60c4dc19319
Nadine <nfarah86@gmail.com>
2015-01-29 08:10:18 -0800

    working with sessions and current_user

--- app/helpers/log_sessions_helper.rb
+++ app/helpers/log_sessions_helper.rb
@@ -1,2 +1,14 @@
 module LogSessionsHelper
+
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+	def current_user_in_session
+		if @current_user.nil?
+			@current_user = User.find_by(id: session[:user_id])
+		else
+			@current_user
+		end
+	end
 end
---
+++ test/integration/login_sessions_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class LoginSessionsTest < ActionDispatch::IntegrationTest
+  
+ test "login with invalid information" do
+    get login_path
+    assert_template 'log_sessions/new'
+    post login_path, session: { email: " ", password: "" }
+    assert_template 'log_sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+end
+
+
+
